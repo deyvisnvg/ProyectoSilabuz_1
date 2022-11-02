92afc79d138d20f4da98a774061e85ba5cd2424d
deyvisnvg <deyvisnvg@gmail.com>
2022-11-02 14:02:26 -0500

    dnvg: Estructura incial del proyecto

---
+++ final1.py
@@ -0,0 +1,39 @@
+class Libro:
+
+    def __init__(self, id, titulo, genero, ISBN, editorial, autores):
+        self.id = id
+        self.titulo = titulo
+        self.genero = genero
+        self.ISBN = ISBN
+        self.editorial = editorial
+        self.autores = autores
+
+    def leer_archivo(self):
+        pass
+
+    def listar_libro(self):
+        pass
+
+    def agregar_libro(self):
+        pass
+
+    def eliminar_libro(self):
+        pass
+
+    def buscar_libro(self):
+        pass
+
+    def ordenar_titulo(self):
+        pass
+
+    def buscar_libro2(self):
+        pass
+
+    def buscar_libro3(self):
+        pass
+
+    def editar_libro(self):
+        pass
+
+    def guardar_libro(self):
+        pass
