2f49dfd5a320929ca92e4c94e9b9f6c8737fcb0b
deyvisnvg <deyvisnvg@gmail.com>
2022-11-05 19:07:33 -0500

    dnvg: metodo list_poke_generacion implementado

--- final2.py
+++ final2.py
@@ -17,9 +17,19 @@ Menu = '''
 
 
 class PokeApi:
+    detail_pokemon = "pokemon"
+    limit = '1154'
+
     def __init__(self, api):
         self.API = api
-        self.lista_generation = []
+        self.lista_name_pokemons = []
+
+    def obtener_lista_pokemons(self):
+        lista_pokemons = self.obtener_pokemon(
+            self.detail_pokemon + "/" + f"?offset=0&limit={self.limit}")
+
+        for pokemon in lista_pokemons['results']:
+            self.lista_name_pokemons.append(pokemon['name'])
 
     def obtener_pokemon(self, detail_pokemon):
         response = requests.get(self.API + detail_pokemon)
@@ -36,25 +46,30 @@ class PokeApi:
         print(Title)
 
         detail = "generation"
-        generation = input("\tIngrese alguna generación: ")
+        generation = input("\tIngrese alguna generación Ej.[1 al 8]: ")
+
+        while int(generation) > 8:
+            generation = input(
+                "\tLa opcion no es correcta, vuelva a ingresar Ej.[1 al 8]: ")
 
         detail_generation = detail + "/" + generation
+        lista_generation = self.obtener_pokemon(detail_generation)
 
-        self.lista_generation = self.obtener_pokemon(detail_generation)
+        resul_pokemons = []
+        for pokemon in lista_generation["pokemon_species"]:
+            if pokemon['name'] in self.lista_name_pokemons:
+                poke = self.obtener_pokemon(
+                    self.detail_pokemon + "/" + pokemon['name'])
+                resul_pokemons.append({
+                    "name": pokemon['name'],
+                    "ability": [abiliy['ability']['name'] for abiliy in poke['abilities']],
+                    "url_imagen": poke['sprites']['front_default']
+                })
 
-        lista_pokemons = []
-        for pokemon in self.lista_generation["pokemon_species"][:10]:
-            pokemon_species = self.obtener_pokemon(pokemon['url'].replace(self.API, ""))
-            pokemon = self.obtener_pokemon(pokemon_species["varieties"][0]["pokemon"]['url'].replace(self.API, ""))
-
-            lista_pokemons.append({
-                "name": pokemon['name'],
-                # 'ability': 
-                "ability": [abiliy['ability']['name'] for abiliy in pokemon['abilities']]
-            })
-
-        for pokemon in lista_pokemons:
+        for pokemon in resul_pokemons:
             self.print_pokemon(pokemon)
+
+        self._continuar()
 
     def list_poke_forma(self):
         pass
@@ -69,16 +84,22 @@ class PokeApi:
         pass
 
     def print_pokemon(self, pokemon):
-        titulo = f"\n\tNombre {pokemon['name']}:\n"
+        titulo = f"\n\tNombre: {pokemon['name']}:\n"
         cuerpo = (f"\tHabilidad: {', '.join(pokemon['ability'])}\n"
-                    f"\tUrl imagen: ""\n")
+                  f"\tUrl imagen: {pokemon['url_imagen']}""\n")
         espacio = "\t*******************" * 3
-        
+
         print(titulo + cuerpo + espacio)
-        
+
     def _continuar(self):
-        pass
-    
+        respuesta = input("\nDesea continuar? S/N: ")
+
+        while respuesta.upper() not in ("S", "N"):
+            respuesta = input(
+                "Debes responder s ó n. Ingresa nuevamente tu respuesta: ")
+
+        return respuesta
+
 
 def run():
     print(Bienvenida)
@@ -86,6 +107,7 @@ def run():
     API = "https://pokeapi.co/api/v2/"
 
     pokeAPi = PokeApi(API)
+    pokeAPi.obtener_lista_pokemons()
 
     while True:
         print(Menu)
